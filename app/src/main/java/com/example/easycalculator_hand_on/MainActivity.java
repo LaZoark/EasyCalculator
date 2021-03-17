86f81841f662dd7cd306db43567b63206f0fec06
LaZoark <emersonchen086@icloud.com>
2021-03-17 22:59:45 +0800

    at least it won't crash...

--- app/src/main/java/com/example/easycalculator_hand_on/MainActivity.java
+++ app/src/main/java/com/example/easycalculator_hand_on/MainActivity.java
@@ -4,50 +4,70 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.NumberPicker;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements RadioGroup.OnCheckedChangeListener, TextWatcher {
+    RadioGroup chooseMode;
+    RadioGroup selectOperand;
+    EditText b1, b2;
+    TextView test, Ans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test = (TextView) findViewById(R.id.textView6);
+        chooseMode = (RadioGroup) findViewById(R.id.radioGroup1);
+        chooseMode.setOnCheckedChangeListener(this);
+        selectOperand = (RadioGroup) findViewById(R.id.radioGroup2);
+        selectOperand.setOnCheckedChangeListener(this);
+        b1 = (EditText) findViewById(R.id.argv1);
+        b1.addTextChangedListener(this);
+        b2 = (EditText) findViewById(R.id.argv2);
+        b2.addTextChangedListener(this);
     }
 
-    RadioButton radioBut1, radioBut2;
-    RadioGroup radioGroup;
-    TextView mainText, mode1Ans, mode2Ans;
-    NumberPicker b1, b2;
-
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+    @Override
+    public void afterTextChanged(Editable s) {
+    }
     @SuppressLint("NonConstantResourceId")
-    public void onRadioButtonClicked(View view) {
-        boolean checked = ((RadioButton) view).isChecked();
-        switch (view.getId()) {
-            case R.id.radioButton:
-                if (checked) {
-                    mode1Ans = (TextView) findViewById(R.id.textView3);
-                    b1 = (NumberPicker) findViewById(R.id.editView);
-                    b2 = (NumberPicker) findViewById(R.id.editView2);
-                    switch (view.getId()) {
-                        case R.id.radioButton_up:
-                            if (checked)
-                                mode1Ans.setText(b1.getValue() + b2.getValue());
-                            break;
-                        case R.id.radioButton_down:
-                            if (checked)
-                                mode1Ans.setText(b1.getValue() - b2.getValue());
-                            break;
-                    }
-                }
-                break;
-            case R.id.radioButton2:
-                if (checked)
-
-                    break;
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        RadioGroup method = (RadioGroup) group;
+        Ans = (TextView) findViewById(R.id.textView3);
+        switch (method.getCheckedRadioButtonId()) {
+            case R.id.plusMinus:
+                switch (method.getCheckedRadioButtonId()) {
+                    case R.id.radioButton_up:
+                        Ans.setText(Integer.parseInt(b1.getText().toString()) + Integer.parseInt(b2.getText().toString()));
+                        break;
+                    case R.id.radioButton_down:
+                        Ans.setText(Integer.parseInt(b1.getText().toString()) - Integer.parseInt(b2.getText().toString()));
+                        break;
+                }break;
+            case R.id.multiplyDivide:
+                switch (method.getCheckedRadioButtonId()) {
+                    case R.id.radioButton_up:
+                        Ans.setText(Integer.parseInt(b1.getText().toString()) * Integer.parseInt(b2.getText().toString()));
+                        break;
+                    case R.id.radioButton_down:
+                        Ans.setText(Integer.parseInt(b1.getText().toString()) / Integer.parseInt(b2.getText().toString()));
+                        break;
+                }break;
         }
     }
+
 }
